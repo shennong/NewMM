2f7208f31644214b47c9f12e5f86442ab622196c
shennong <Changanq@gmail.com>
2013-09-05 20:17:15 +0800

    添加的第一个项目

---
+++ ServGen/ServGen.h
@@ -0,0 +1,31 @@
+// ServGen.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CServGenApp:
+// 有关此类的实现，请参阅 ServGen.cpp
+//
+
+class CServGenApp : public CWinApp
+{
+public:
+	CServGenApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CServGenApp theApp;
